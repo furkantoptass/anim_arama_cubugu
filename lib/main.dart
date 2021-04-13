393ce88dccdc28615b06cd1bfcbf2ef76f17db3c
Furkan Toptas <toptas.furkan@gmail.com>
2021-04-13 14:13:37 +0300

    Gereksinimimlere göre tasarım değiştirildi.
    Aranan kelimeye göre arkaplan rengi değiştiren bir uygulama için bütün değişikler yapıldı.

--- lib/main.dart
+++ lib/main.dart
@@ -23,25 +23,61 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   TextEditingController textController = TextEditingController();
+  String aranan = ""; //Arama çubuguna girilen değer
+  String arananEkran = ""; //Ekranda gösterilen Arama çubugundan alınan veri
+  Color color = Colors.deepOrange[100]; //Arkaplan rengi değiştirmek için
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: color, //arka plan rengini biz belirliyoruz.
       appBar: AppBar(
         title: Text("Webmaster.Kitchen"),
         backgroundColor: Colors.deepOrangeAccent,
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 58.0, right: 10, left: 10),
-
-        /// In AnimSearchBar widget, the width, textController, onSuffixTap are required properties.
-        /// You have also control over the suffixIcon, prefixIcon, helpText and animationDurationInMilli
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Text("Animasyonlu Arama Çubuğu",
+                style: Theme.of(context).textTheme.headline5),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            //Aşağıda arama yapıldıgında arama yapılan kelimeyi ekran da göstrecektir.
+            child: Center(
+              child: Text("$arananEkran",
+                  style: Theme.of(context).textTheme.overline),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(top: 50.0, right: 10, left: 10),
         child: AnimSearchBar(
-          width: 400,
-          textController: textController,
+          width: 400, //Arama çubuğu uzunlugu için düzenlenebilir
+          helpText:
+              "Arama...", //Arama bölümüne giriş olmadığında görülecek olan yazı
+          textController: textController, //Girilen kelimeler
+          suffixIcon: Icon(Icons.search), //Arama simgesi
+
+          //Arama simgesine tıklama olayı aşağıdadır.
           onSuffixTap: () {
             setState(() {
-              textController.clear();
+              aranan = textController.text
+                  .toString(); //Girilen kelimeyi aranan adlı değişkenine atıyoruz.
+
+              if (aranan ==
+                  "webmasterkitchen") //Girilen kelime webmasterkitchen a eşitse
+              {
+                color = Colors.amber[100]; //Arkaplan rengini değiştiriyoruz.
+              } else {
+                color = Colors.deepOrange[100]; //Webmasterkitchen a eşit değise
+              }
+              arananEkran =
+                  "Aranan Kelime : " + aranan; //normal rengini veriyoruz.
+              textController
+                  .clear(); //Arama sonrası text inputun içini siliyoruz.
             });
           },
         ),
